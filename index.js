6a07d5cf2e7db2f2cb1f166d3848d256b50eecb4
Tuomoes <tuomo_eskola@hotmail.com>
2018-06-29 14:45:50 +0300

    subtask 3.1 in progress (missing auto restart)

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log('hello world')
+const http = require('http')
+
+let persons = [
+    {
+        name: "Arto Hellas",
+        number: "040-12542",
+        id: 1
+    },
+    {
+        name: "Matti Tienari",
+        number: "040-234567",
+        id: 2
+    },
+    {
+        name: "Arto Järvinen",
+        number: "040-98765",
+        id: 3
+    },
+    {
+        name: "Lea Kutvonen",
+        number: "040-356893",
+        id: 4
+    }
+]
+
+const app = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' })
+    res.end(JSON.stringify(persons))
+})
+
+const port = 3001
+app.listen(port)
+console.log('Server is running on port', port)
