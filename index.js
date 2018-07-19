963dd2eee0f67a9a04d34af9778b7c4ba896817b
tuomo <tuomo_eskola@hotmail.com>
2018-07-19 10:42:06 +0300

    subtask 3.16 done

--- index.js
+++ index.js
@@ -91,10 +91,16 @@ app.get('/api/persons/:id', (req, res) => {
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(person => person.id !== id)
-
-    res.status(204).end()
+    //const id = Number(req.params.id)
+    //persons = persons.filter(person => person.id !== id)
+    Person
+        .findByIdAndRemove(req.params.id)
+        .then(result => {
+            res.status(204).end()
+        })
+        .catch(error => {
+            res.status(400).send({ error: 'malformatted id' })
+        })
 })
 
 app.post('/api/persons', (req, res) => {
